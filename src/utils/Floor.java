5b53b83fc017710b98990b974db5688fbcc80f2c
cyrilico <jpdamas15@gmail.com>
2018-11-01 15:43:14 +0000

    BAMUS design

--- src/utils/Floor.java
+++ src/utils/Floor.java
@@ -2,14 +2,30 @@ package utils;
 
 import uchicago.src.sim.gui.Drawable;
 import uchicago.src.sim.gui.SimGraphics;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Floor implements Drawable {
-    private final int lift_speed;
+    private static BufferedImage up = null;
+    private static BufferedImage down = null;
+    private int lift_speed;
     private int x;
     private int y;
     private int counter = 0;
     private Direction direction;
+
+    static{
+        try {
+            up = ImageIO.read(new File("assets/up.png"));
+            down = ImageIO.read(new File("assets/down.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public Floor(int x, int y, int lift_speed) {
         this.x = x;
@@ -22,9 +38,7 @@ public class Floor implements Drawable {
     public void draw(SimGraphics simGraphics) {
         simGraphics.setFont(new Font("Arial", Font.BOLD, 40));
         if (counter > 0) {
-            simGraphics.drawRect(this.direction == Direction.DOWN ? Color.GREEN : Color.BLUE);
-            //simGraphics.drawStringInRect(Color.GREEN,Color.BLUE,this.direction == Direction.DOWN ? "v" : "^");
-            simGraphics.drawString("ALIVE", Color.BLUE);
+            simGraphics.drawImageToFit(direction == Direction.DOWN ? Floor.down : Floor.up);
             counter = (counter + 1) % lift_speed;
         } else
             simGraphics.drawRect(Color.RED);
